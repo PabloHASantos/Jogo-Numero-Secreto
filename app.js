3f57c3c729080623308232d3e1f296ab0f58885c
Pablo Henrique <pblohasantos@gmail.com>
2025-02-12 13:56:50 -0300

    Aumentado o limite

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumeroSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag, texto) {
 // essa função vai exibir a mensagem inicial
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número de 1 a 10: ');
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 100: ');
 }
 
 exibirMensagemInicial();
